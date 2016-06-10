cf30ba8b00948b2cf24fef07714fbad42c39ef50
odino <alessandro.nadalin@gmail.com>
2016-06-11 02:25:11 +0400

    Added a welcome message to know where to connect to

--- main.go
+++ main.go
@@ -4,7 +4,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"net"
 	"net/http"
 	"os"
 
@@ -26,11 +28,46 @@ func getPort() string {
 	return port
 }
 
+// Get our local IP address:
+// so that the remote controller
+// can connect to this address + port
+// to access us
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+
+	return ""
+}
+
+// Simple helper function to print the
+// address from which you can RC us
+func welcome(ip string, port string) {
+	if ip == "" {
+		fmt.Print("Unable to get local IP address. Are we connected to a network?")
+	} else {
+		fmt.Printf("Aye, here we are: connect to http://%s:%s and enjoy!", ip, port)
+	}
+}
+
 // Have fun!
 func main() {
+	localIp := getLocalIP()
+	port := getPort()
+
+	welcome(localIp, port)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/press/{keys}", h.PressHandler)
 
 	http.Handle("/", r)
-	http.ListenAndServe(":"+getPort(), nil)
+	http.ListenAndServe(":"+port, nil)
 }
